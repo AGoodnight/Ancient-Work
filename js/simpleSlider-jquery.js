b13cec46dd69ee39318bc397e46a7c5c49616aee
Goodnightmg <agoodnight@mediag.com>
2014-07-02 11:04:44 -0400

    Finished Jquery Plug-In tutorial
    Practiced the best practices

---
+++ js/simpleSlider-jquery.js
@@ -0,0 +1,23 @@
+(function($){
+
+	$.fn.simpleSlider = function(options){
+		
+		// the blank object ( {} ) prevents our defaults from being overwritten.
+		var settings = $.extend({},$.fn.simpleSlider.defaults ,options);	
+		
+		function log(obj){
+			
+		}
+			
+		return this
+		
+	};
+	
+	// isolating the defaults and then calling them in the extend above gives users the ability to set defaults at any time.
+	$.fn.simpleSlider.defaults ={
+		speed:1,
+		delay:3,
+		auto:true
+	}
+
+}(jQuery));
